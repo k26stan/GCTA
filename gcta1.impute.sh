359da6e82bc17764528877690cb98f5dfa5edc2c
Kristopher Standish <k26stan@yahoo.com>
2015-01-27 20:39:05 -0800

    Reorganize the directory
    Moved Nathan’s scripts into a designated folder

--- gcta1.impute.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-GCTA=/gpfs/group/schork/nwineing/gcta/gcta64
-DIR=/gpfs/group/schork/nwineing/jnj/gcta
-${GCTA} --bfile /gpfs/group/schork/nwineing/jnj/gcta/datasets/combined.info5 --autosome --make-grm --out ${DIR}/combined.info5
-
--- gcta2.impute.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-GCTA=/gpfs/group/schork/nwineing/gcta/gcta64
-DIR=/gpfs/group/schork/nwineing/jnj/gcta
-#${GCTA} --grm ${DIR}/combined.info5 --grm-cutoff 0.05 --make-grm --out ${DIR}/combined.info5.rm.05
-
